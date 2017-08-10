7e7ca88319b6cb5eeef7b08234748ae7cc956361
= <=>
2017-08-10 13:34:15 -0300

    Correcao do bug de transportar a base64

--- Backend/API/config/express.js
+++ Backend/API/config/express.js
@@ -3,8 +3,16 @@ var app = express();
 var cors = require('cors')();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	limit: '5mb',
+	parameterLimit: 100000,
+    extended: false 
+}));
+
+app.use(bodyParser.json({
+	limit: '5mb'
+}));
+
 app.use(cors);  
 
 module.exports = function configura(){
