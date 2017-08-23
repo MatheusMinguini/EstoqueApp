1c913981d3f6a9173b6cef42dbbd15d8fe0caf19
= <=>
2017-08-23 13:59:14 -0300

    Melhorias

--- src/pages/resultado/resultado.ts
+++ src/pages/resultado/resultado.ts
@@ -5,10 +5,11 @@ import { EscolhaPage } from '../escolha/escolha';
 import { FormularioPage } from '../formulario/formulario';
 import { PesquisaPage } from '../pesquisa/pesquisa';
 import { Produto } from '../../models/Produto'
-
+import { Configuracao } from '../../services/config.service';
 
 
 @Component({
+  providers: [Configuracao],
   templateUrl: 'resultado.html'
 })
 export class ResultadoPage implements OnInit{
@@ -20,13 +21,18 @@ export class ResultadoPage implements OnInit{
               private _http: Http,
               private _loadingCtrl: LoadingController,
               private _alertCtrl: AlertController,
-              public parametro : NavParams
-              ) {
+              public parametro : NavParams,
+              public _configuracao : Configuracao) {
 
   }
 
   ngOnInit(){
-      this.produtos = this.parametro.get('produtosEncontrados');
+     this.produtos = this.parametro.get('produtosEncontrados');
+
+    this._mensagem = this._alertCtrl.create({
+      title : 'Aviso',
+      buttons : [{ text : "Tudo bem"}]
+    });
   }
 
   seleciona(produto: Object){
@@ -41,18 +47,40 @@ export class ResultadoPage implements OnInit{
     this.navCtrl.push(PesquisaPage);
   }
 
-  remover(evento){
-    let api = 'http://localhost:3010/remover';
+  remover(produto){
 
-    this._http.post(api, evento)
+    var index = this.produtos.indexOf(produto);
+    this.produtos.splice(index, 1);
+
+    this._http.post(this._configuracao.getAdressAPI() + '/remover', produto)
       .map(resp => resp.json())
         .toPromise().then(elemento => {
-           this._mensagem.setSubTitle('Evento removido');
+           this._mensagem.setSubTitle('Produto removido');
            this._mensagem.present();
         }).catch (erro => {
             this._mensagem.setSubTitle('Ocorreu algum problema. Tente mais tarde');
             this._mensagem.present();
             console.log(erro);
         });
+  }
+
+  confirmarOperacao(objeto) {
+    let alert = this._alertCtrl.create({
+      title: 'Confirmar Operação',
+      message: 'Você vendeu o produto e deseja excluí-lo?',
+      buttons: [
+        {
+          text: 'Não excluir',
+          role: 'cancel'
+        },
+        {
+          text: 'Excluir',
+          handler: () => {
+            this.remover(objeto);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
